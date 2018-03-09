bc104924256428df4791cbe80127c19af400621f
Ryan Schmelter <rschmelter33@gmail.com>
2018-03-09 19:34:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,8 +20,8 @@ class ApplicationController < Sinatra::Base
     end
 
   get '/account' do
-      @user = User.find_by_id(session[:user_id])
-      if @user
+      @new_user = User.find_by_id(session[:user_id])
+      if @new_user
       erb :account
     else
       erb :error
